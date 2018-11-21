d26f3c2daeec9aaa7e6c782b2ddb8e104e621f64
Wan Youwen <1756429307@qq.com>
2018-11-21 11:10:18 +0800

    g2o Mat Eigen 李代数 数据类型转换

--- include/Converter.h
+++ include/Converter.h
@@ -1,21 +1,6 @@
 /**
 * This file is part of ORB-SLAM2.
-*
-* Copyright (C) 2014-2016 Raúl Mur-Artal <raulmur at unizar dot es> (University of Zaragoza)
-* For more information see <https://github.com/raulmur/ORB_SLAM2>
-*
-* ORB-SLAM2 is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* ORB-SLAM2 is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with ORB-SLAM2. If not, see <http://www.gnu.org/licenses/>.
+
 */
 
 #ifndef CONVERTER_H
@@ -33,22 +18,28 @@ namespace ORB_SLAM2
 class Converter
 {
 public:
+   // 矩阵形式的描述子 转向量形式的描述子
     static std::vector<cv::Mat> toDescriptorVector(const cv::Mat &Descriptors);
 
+   // 4*4 mat形式的 位姿态  R t 转化成 G2O 6自由度顶点优化变量类型  李代数形式 SE3Quat  
     static g2o::SE3Quat toSE3Quat(const cv::Mat &cvT);
     static g2o::SE3Quat toSE3Quat(const g2o::Sim3 &gSim3);
 
+    // g2o 类型转Mat=======
     static cv::Mat toCvMat(const g2o::SE3Quat &SE3);
     static cv::Mat toCvMat(const g2o::Sim3 &Sim3);
-    static cv::Mat toCvMat(const Eigen::Matrix<double,4,4> &m);
+
+     // Eigen 转 Mat========
+    static cv::Mat toCvMat(const Eigen::Matrix<double,4, 4> &m);
     static cv::Mat toCvMat(const Eigen::Matrix3d &m);
     static cv::Mat toCvMat(const Eigen::Matrix<double,3,1> &m);
-    static cv::Mat toCvSE3(const Eigen::Matrix<double,3,3> &R, const Eigen::Matrix<double,3,1> &t);
-
+    static cv::Mat toCvSE3(const Eigen::Matrix<double,3,3> &R, 
+                                                  const Eigen::Matrix<double,3,1> &t);
+ // Mat 转 Eigen============================
     static Eigen::Matrix<double,3,1> toVector3d(const cv::Mat &cvVector);
     static Eigen::Matrix<double,3,1> toVector3d(const cv::Point3f &cvPoint);
     static Eigen::Matrix<double,3,3> toMatrix3d(const cv::Mat &cvMat3);
-
+// 转四元素==========
     static std::vector<float> toQuaternion(const cv::Mat &M);
 };
 
