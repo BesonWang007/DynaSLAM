1d6362539225a20a66fc3eeeb6f865afd532d866
Wan Youwen <1756429307@qq.com>
2018-11-21 13:01:56 +0800

    地图 管理 关键帧 地图点 添加/删除  关键帧/地图点 获取所有关键帧 地图点 参考地图点

--- include/Map.h
+++ include/Map.h
@@ -1,21 +1,8 @@
 /**
 * This file is part of ORB-SLAM2.
-*
-* Copyright (C) 2014-2016 Raúl Mur-Artal <raulmur at unizar dot es> (University of Zaragoza)
-* For more information see <https://github.com/raulmur/ORB_SLAM2>
-*
-* ORB-SLAM2 is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* ORB-SLAM2 is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with ORB-SLAM2. If not, see <http://www.gnu.org/licenses/>.
+* 地图 管理 关键帧 地图点
+*  添加/删除  关键帧/地图点
+* 
 */
 
 #ifndef MAP_H
@@ -26,35 +13,33 @@
 #include <set>
 
 #include <mutex>
-
-
-
+// 地图
 namespace ORB_SLAM2
 {
 
 class MapPoint;
 class KeyFrame;
 
+
 class Map
 {
 public:
     Map();
 
-    void AddKeyFrame(KeyFrame* pKF);
-    void AddMapPoint(MapPoint* pMP);
-    void EraseMapPoint(MapPoint* pMP);
-    void EraseKeyFrame(KeyFrame* pKF);
-    void SetReferenceMapPoints(const std::vector<MapPoint*> &vpMPs);
-    void InformNewBigChange();
-    int GetLastBigChangeIdx();
+    void AddKeyFrame(KeyFrame* pKF);// 添加关键帧
+    void AddMapPoint(MapPoint* pMP);// 添加地图点
+    void EraseMapPoint(MapPoint* pMP);// 删除地图点
+    void EraseKeyFrame(KeyFrame* pKF);//删除关键帧
+    void SetReferenceMapPoints(const std::vector<MapPoint*> &vpMPs);// 参考地图点
+    void InformNewBigChange();// 地图数据 大变化
+    int GetLastBigChangeIdx();// 上次地图数据大变化 id
 
-    std::vector<KeyFrame*> GetAllKeyFrames();
-    std::vector<MapPoint*> GetAllMapPoints();
+    std::vector<KeyFrame*> GetAllKeyFrames();// 得到所有关键帧
+    std::vector<MapPoint*> GetAllMapPoints();// 得到所有地图点
     std::vector<MapPoint*> GetReferenceMapPoints();
 
-    long unsigned int MapPointsInMap();
-    long unsigned  KeyFramesInMap();
-
+    long unsigned int MapPointsInMap();// 地图中地图点的数量
+    long unsigned  KeyFramesInMap();// 关键帧数量
     long unsigned int GetMaxKFid();
 
     void clear();
@@ -66,18 +51,21 @@ public:
     // This avoid that two points are created simultaneously in separate threads (id conflict)
     std::mutex mMutexPointCreation;
 
-protected:
-    std::set<MapPoint*> mspMapPoints;
-    std::set<KeyFrame*> mspKeyFrames;
+protected://类内访问变量
+    std::set<MapPoint*> mspMapPoints;//所有地图点
+    std::set<KeyFrame*> mspKeyFrames;//所有关键帧
 
-    std::vector<MapPoint*> mvpReferenceMapPoints;
+    std::vector<MapPoint*> mvpReferenceMapPoints;//所有参考地图点 上一帧
 
     long unsigned int mnMaxKFid;
 
     // Index related to a big change in the map (loop closure, global BA)
     int mnBigChangeIdx;
 
-    std::mutex mMutexMap;
+    std::mutex mMutexMap;// 线程 互斥锁
+    // 其中std::mutex就是lock、unlock。std::lock_guard与std::mutex配合使用，把锁放到lock_guard中时，
+    // mutex自动上锁，lock_guard析构时，同时把mutex解锁。mutex又称互斥量。
+    
 };
 
 } //namespace ORB_SLAM
