0b6145e1284a81e3344582439cf21475befee820
Wan Youwen <1756429307@qq.com>
2018-11-21 11:46:08 +0800

    几何学 几何模型修正 深度数据关键点区域增长更新 ===================

--- include/Geometry.h
+++ include/Geometry.h
@@ -1,8 +1,8 @@
 /**
 * This file is part of DynaSLAM.
-* Copyright (C) 2018 Berta Bescos <bbescos at unizar dot es> (University of Zaragoza)
+* Copyright (C) 2018 Berta Bescos <bbescos at unizar dot es> (University of Zaragoza) 萨拉戈萨大学
 * For more information see <https://github.com/bertabescos/DynaSLAM>.
-*
+* 几何学 几何模型修正 深度数据关键点区域增长更新
 */
 
 #ifndef GEOMETRY_H
@@ -16,7 +16,7 @@
 #include <opencv2/features2d/features2d.hpp>
 #include "Frame.h"
 
-#define MAX_DB_SIZE 20
+#define MAX_DB_SIZE 20// 数据库 20类物体  voc数据集???
 #define MAX_REF_FRAMES 5
 #define ELEM_INITIAL_MAP 5
 #define MIN_DEPTH_THRESHOLD 0.2
@@ -27,34 +27,47 @@ namespace DynaSLAM
 class Geometry
 {
 private:
-    class DynKeyPoint
+
+    class DynKeyPoint // 关键点
     {
     public:
-        cv::Point2i mPoint;
-        int mRefFrameLabel;
+        cv::Point2i mPoint;     // 2d像素点
+        int mRefFrameLabel;// 标签
     };
-    class DataBase
+
+    class DataBase// 数据库
     {
     public:
-        vector<ORB_SLAM2::Frame> mvDataBase = vector<ORB_SLAM2::Frame>(MAX_DB_SIZE);
+        vector<ORB_SLAM2::Frame> mvDataBase = vector<ORB_SLAM2::Frame>(MAX_DB_SIZE); 
         int mIni=0;
         int mFin=0;
         int mNumElem = 0;
         bool IsFull();
-        void InsertFrame2DB(const ORB_SLAM2::Frame &currentFrame);
+        void InsertFrame2DB(const ORB_SLAM2::Frame &currentFrame);// 向 数据库插入关键帧 ====
     };
-    vector<DynKeyPoint> ExtractDynPoints(const vector<ORB_SLAM2::Frame> &vRefFrames, const ORB_SLAM2::Frame &currentFrame);
+// 提取关键点================
+    vector<DynKeyPoint> ExtractDynPoints(const vector<ORB_SLAM2::Frame> &vRefFrames, 
+                                                                                    const ORB_SLAM2::Frame &currentFrame);
+// 获取参考帧================
     vector<ORB_SLAM2::Frame> GetRefFrames(const ORB_SLAM2::Frame &currentFrame);
+// 结合 掩码mask=============
     void CombineMasks(const ORB_SLAM2::Frame &currentFrame, cv::Mat &mask);
-    void FillRGBD(const ORB_SLAM2::Frame &currentFrame,cv::Mat &mask,cv::Mat &imGray,cv::Mat &imDepth);
-    void FillRGBD(const ORB_SLAM2::Frame &currentFrame,cv::Mat &mask,cv::Mat &imGray,cv::Mat &imDepth,cv::Mat &imRGB);
+// 填充 ====================？
+    void FillRGBD(const ORB_SLAM2::Frame &currentFrame,
+                                cv::Mat &mask,cv::Mat &imGray,cv::Mat &imDepth);
+    void FillRGBD(const ORB_SLAM2::Frame &currentFrame,
+                                cv::Mat &mask,cv::Mat &imGray,cv::Mat &imDepth,cv::Mat &imRGB);// 多一张彩色图==
 
-
-    cv::Mat DepthRegionGrowing(const vector<DynKeyPoint> &vDynPoints,const cv::Mat &imDepth);
+// 利用深度数据对关键点进行区域增长更新================
+    cv::Mat DepthRegionGrowing(const vector<DynKeyPoint> &vDynPoints,
+                                                                const cv::Mat &imDepth);
 
     bool isRotationMatrix(const cv::Mat &R);
-    cv::Mat rotm2euler(const cv::Mat &R);
-    cv::Mat RegionGrowing(const cv::Mat &Image,int &x,int &y,const float &threshold);
+    cv::Mat rotm2euler(const cv::Mat &R);// 旋转矩阵 转 欧拉角
+
+// 阈值区域增长======
+    cv::Mat RegionGrowing(const cv::Mat &Image,
+                                                   int &x,int &y,const float &threshold);
 
     cv::Mat RegionGrowingGaps(const cv::Mat &Image, int &x, int &y);
 
@@ -62,9 +75,9 @@ private:
 
     int mDmax;
 
-    float mDepthThreshold;
+    float mDepthThreshold;// 深度阈值
 
-    float mSegThreshold;
+    float mSegThreshold; // 分割阈值
 
     double mSizeThreshold;
 
@@ -76,15 +89,24 @@ private:
 
     cv::Mat vAllPixels;
 
-    bool IsInFrame(const float &x, const float &y, const ORB_SLAM2::Frame &Frame);
+    bool IsInFrame(const float &x, const float &y, 
+                                     const ORB_SLAM2::Frame &Frame);
+
     bool IsInImage(const float &x, const float &y, const cv::Mat image);
-    void GetClosestNonEmptyCoordinates(const cv::Mat &mask, const int &x, const int &y, int &_x, int &_y);
+// 非空 候选者======
+    void GetClosestNonEmptyCoordinates(const cv::Mat &mask, 
+                                                                                   const int &x, const int &y, int &_x, int &_y);
 
 public:
     Geometry();
     ~Geometry() = default;
-    void GeometricModelCorrection(const ORB_SLAM2::Frame &currentFrame, cv::Mat &imDepth, cv::Mat &mask);
-    void InpaintFrames(const ORB_SLAM2::Frame &currentFrame, cv::Mat &imGray, cv::Mat &imDepth, cv::Mat &imRGB, cv::Mat &mask);
+// 几何模型修正 ====================
+    void GeometricModelCorrection(const ORB_SLAM2::Frame &currentFrame, 
+                                                                       cv::Mat &imDepth, cv::Mat &mask);
+// 画框 ========?
+    void InpaintFrames(const ORB_SLAM2::Frame &currentFrame, 
+                                             cv::Mat &imGray, cv::Mat &imDepth, cv::Mat &imRGB, cv::Mat &mask);
+// 更新数据库=====
     void GeometricModelUpdateDB(const ORB_SLAM2::Frame &mCurrentFrame);
 };
 
