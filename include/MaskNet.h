d887ba373a9926ccd7d94c70beb1b7c82da30c8a
Wan Youwen <1756429307@qq.com>
2018-11-21 13:13:20 +0800

    mask-rcnn 实现的语义分割 主要是 python接口 需要做一些数据转换 语义分割结果

--- include/MaskNet.h
+++ include/MaskNet.h
@@ -2,7 +2,9 @@
 * This file is part of DynaSLAM.
 * Copyright (C) 2018 Berta Bescos <bbescos at unizar dot es> (University of Zaragoza)
 * For more information see <https://github.com/bertabescos/DynaSLAM>.
-*
+* mask-rcnn 实现的语义分割= 
+主要是 python接口=
+需要做一些数据转换====
 */
 
 #ifndef __MASKNET_H
@@ -30,11 +32,11 @@ namespace DynaSLAM
 
 class SegmentDynObject{
 private:
-	NDArrayConverter *cvt; 	/*!< Converter to NumPy Array from cv::Mat */
-	PyObject *py_module; 	/*!< Module of python where the Mask algorithm is implemented */
-	PyObject *py_class; 	/*!< Class to be instanced */
+	NDArrayConverter *cvt; 	/*!<  cv::Mat  转换到  NumPy Array  */
+	PyObject *py_module; 	/*!< python 模块 Module of python where the Mask algorithm is implemented */
+	PyObject *py_class; 	        /*!< Class to be instanced */
 	PyObject *net; 			/*!< Instance of the class */
-	std::string py_path; 	/*!< Path to be included to the environment variable PYTHONPATH */
+	std::string py_path; 	        /*!< PYTHONPATH 变量路径 */
 	std::string module_name; /*!< Detailed description after the member */
 	std::string class_name; /*!< Detailed description after the member */
         std::string get_dyn_seg; 	/*!< Detailed description after the member */
@@ -44,6 +46,7 @@ public:
 
 	SegmentDynObject();
         ~SegmentDynObject();
+// 获取检测结果 语义分割结果=======
         cv::Mat GetSegmentation(cv::Mat &image, std::string dir="no_save", std::string rgb_name="no_file");
 };
 
