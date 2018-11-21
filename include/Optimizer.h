14ad7f9b985218ad64c9d5df10ed133bde77ee49
Wan Youwen <1756429307@qq.com>
2018-11-21 13:29:50 +0800

    全局/局部 优化 使用G2O图优化

--- include/Optimizer.h
+++ include/Optimizer.h
@@ -1,21 +1,6 @@
 /**
 * This file is part of ORB-SLAM2.
-*
-* Copyright (C) 2014-2016 Raúl Mur-Artal <raulmur at unizar dot es> (University of Zaragoza)
-* For more information see <https://github.com/raulmur/ORB_SLAM2>
-*
-* ORB-SLAM2 is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* ORB-SLAM2 is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with ORB-SLAM2. If not, see <http://www.gnu.org/licenses/>.
+* 全局/局部 优化 使用G2O图优化
 */
 
 #ifndef OPTIMIZER_H
@@ -26,8 +11,13 @@
 #include "KeyFrame.h"
 #include "LoopClosing.h"
 #include "Frame.h"
-#include "Geometry.h"
+
+#include "Geometry.h" // 几何学 几何模型修正 深度数据关键点区域增长更新 ===
+// 好像又没用===
+
 #include "Thirdparty/g2o/g2o/types/types_seven_dof_expmap.h"
+// 定义好的顶点类型  7维度 优化变量  例如 相机 位姿 + 深度信息
+
 
 namespace ORB_SLAM2
 {
@@ -37,18 +27,27 @@ class LoopClosing;
 class Optimizer
 {
 public:
-    void static BundleAdjustment(const std::vector<KeyFrame*> &vpKF, const std::vector<MapPoint*> &vpMP,
-                                 int nIterations = 5, bool *pbStopFlag=NULL, const unsigned long nLoopKF=0,
-                                 const bool bRobust = true);
+  // BA 最小化重投影误差 优化
+    void static BundleAdjustment(const std::vector<KeyFrame*> &vpKF, 
+                                                                   const std::vector<MapPoint*> &vpMP,
+                                                                   int nIterations = 5, bool *pbStopFlag=NULL, 
+                                                                   const unsigned long nLoopKF=0,
+                                                                   const bool bRobust = true);
+    // 全局优化  地图  迭代次数
     void static GlobalBundleAdjustemnt(Map* pMap, int nIterations=5, bool *pbStopFlag=NULL,
                                        const unsigned long nLoopKF=0, const bool bRobust = true);
+    // 局部优化
     void static LocalBundleAdjustment(KeyFrame* pKF, bool *pbStopFlag, Map *pMap);
+    // 位姿 优化
     int static PoseOptimization(Frame* pFrame);
 
     //Inserted by Berta
     //int PoseOptimization(Frame *pFrame,DynaSLAM::Geometry & geom);
 
+    // 优化图 3d位姿 + 3d平移  双目/深度 优化6维
+    // 单目 在优化 3d位姿 + 3d平移上在 优化一维 深度信息
     // if bFixScale is true, 6DoF optimization (stereo,rgbd), 7DoF otherwise (mono)
+    // 闭环检测到后的优化
     void static OptimizeEssentialGraph(Map* pMap, KeyFrame* pLoopKF, KeyFrame* pCurKF,
                                        const LoopClosing::KeyFrameAndPose &NonCorrectedSim3,
                                        const LoopClosing::KeyFrameAndPose &CorrectedSim3,
